2c27e5864b153d57f93f89bb1ff30bbb15a63e3d
Sofi-Chan <dylan.jherico@gmail.com>
2025-11-26 02:39:32 -0500

    Terminado

--- clasificador.py
+++ clasificador.py
@@ -1,6 +1,8 @@
 import cv2
 import glob
 from sklearn import datasets
+from sklearn.neighbors import KNeighborsClassifier
+from collections import Counter
 import numpy as np
 
 #Dylan
@@ -11,7 +13,7 @@ def cortar_imagenes(img):
     izquierda = 0
     derecha = 0
     for fila in img:
-        if np.all(fila > 150):
+        if np.all(fila > 150): 
             arriba += 1
         else: 
             break
@@ -47,12 +49,61 @@ def cortar_imagenes(img):
     return img
 
 imagenes_clasificar = []
+nombres_imagenes = []
+
+#Trabajar Imagenes
+#Adriano
 
 for archivo in glob.glob("imagenes/*.*"):
     img = cv2.imread(archivo, cv2.IMREAD_GRAYSCALE)
+    nombres_imagenes.append(archivo)
     img = cortar_imagenes(img)
     img = cv2.resize(img, (8, 8))
     for i in range(8):
         for j in range(8):
             img[i][j] = (255 - img[i][j])/255*16
-    imagenes_clasificar.append(img)
+    imagenes_clasificar.append(img.flatten())
+
+#Clasificar
+#Matias
+
+digitos = datasets.load_digits()
+KNN = KNeighborsClassifier(n_neighbors=3)
+KNN.fit(digitos.data, digitos.target)
+
+distancias, indices = KNN.kneighbors(imagenes_clasificar, n_neighbors=3)
+#Indices es una matriz N x 3
+
+'''e) Para cada número nuevo, imprima los targets que corresponden los 3 dígitos más
+parecidos a él. Además, indique qué número es realmente. Así podremos tener en
+pantalla su etiqueta verdadera, y los target de los 3 vecinos más cercanos./'''
+
+'''f) Intente clasificar a sus nuevos dígitos:'''
+
+
+for i in range(len(imagenes_clasificar)):
+    print("Imagen", nombres_imagenes[i][9:10], "tiene como targets:", digitos.target[indices[i]])
+    
+    if digitos.target[indices[i]][0] == digitos.target[indices[i]][1] or digitos.target[indices[i]][0] == digitos.target[indices[i]][2]:
+        X = digitos.target[indices[i]][0]
+    elif digitos.target[indices[i]][1] == digitos.target[indices[i]][2]:
+        X = digitos.target[indices[i]][1]
+        
+    #Si no, escogemos al de menor distancia
+    indice_menor_distancia = np.argmin(distancias[i])
+    X = digitos.target[indices[i][indice_menor_distancia]]
+
+    
+    print('"Soy la inteligencia artificial, y he detectado que el dígito ingresado corresponde al número', X, '”, donde', X, 'es un número entre 0 y 9.')
+
+#Matriz Confusion
+#Alejandro
+
+
+
+
+
+
+
+
+
